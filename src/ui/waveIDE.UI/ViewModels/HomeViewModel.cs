2cf143c1e50683293bc08f5f1217f6f305dfd49d
Matthew <mateuspo.fradique@gmail.com>
2026-08-24 16:07:05 +0100

    fix: Disable documentation generation for the UI projects
    I'm my own end user, makes no sense to have documentation warnings for it.

--- src/ui/waveIDE.UI/ViewModels/HomeViewModel.cs
+++ src/ui/waveIDE.UI/ViewModels/HomeViewModel.cs
@@ -1,7 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 
-using OceanApocalypse.Wave.IDE.UI.ViewModels;
-
 namespace OceanApocalypse.Wave.IDE.UI.ViewModels;
 
 public partial class HomeViewModel : ViewModelBase
