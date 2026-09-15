f4453253d41adb54d5030bfe7d0455f2e14adbcf
Matthew <109810178+mf366-dev@users.noreply.github.com>
2026-09-15 21:57:22 +0100

    refactor(core): Move core to separate directory
    This way, it's no longer tied to the extensibility system.

--- src/core/waveIDE.Common/Operations/Result{TOk,TErr}.cs
+++ src/core/waveIDE.Common/Operations/Result{TOk,TErr}.cs
@@ -6,22 +6,22 @@
  * you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
 */
 
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
-namespace OceanApocalypse.Wave.Extensibility.Operations;
+namespace OceanApocalypse.Wave.Operations;
 
 /// <summary>
 /// A result is a 3-state data structure that wraps operations where regular
